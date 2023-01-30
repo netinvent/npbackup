f93b01ec5442a7813d7334e6a5cf0861109cedd2
Orsiris de Jong <ozy@netpower.fr>
2023-01-30 13:26:31 +0100

    Bump version

--- npbackup/__main__.py
+++ npbackup/__main__.py
@@ -9,8 +9,8 @@ __site__ = "https://www.netperfect.fr/npbackup"
 __description__ = "NetPerfect Backup Client"
 __copyright__ = "Copyright (C) 2022-2023 NetInvent"
 __license__ = "GPL-3.0-only"
-__build__ = "2023012601"
-__version__ = "2.1.0"
+__build__ = "2023013001"
+__version__ = "2.1.1"
 
 
 import os
