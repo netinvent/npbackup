de16c9d0a6a0541bcacd08967a7882145c2cd6a1
Orsiris de Jong <ozy@netpower.fr>
2023-02-06 22:24:23 +0100

    Don't import modules on non windows systems

--- npbackup/gui/minimize_window.py
+++ npbackup/gui/minimize_window.py
@@ -7,13 +7,11 @@ __intname__ = "npbackup.gui.window_reducer"
 __author__ = "Orsiris de Jong"
 __copyright__ = "Copyright (C) 2022-2023 NetInvent"
 __license__ = "GPL-3.0-only"
-__build__ = "2023020501"
+__build__ = "2023020601"
 
 
 import sys
 import os
-import win32gui
-import win32con
 
 
 def minimize_current_window():
@@ -21,6 +19,8 @@ def minimize_current_window():
     Minimizes current commandline window in GUI mode
     """
     if os.name == "nt":
+        import win32gui
+        import win32con
         current_executable = os.path.abspath(sys.argv[0])
         # console window will have the name of current executable
         hwndMain = win32gui.FindWindow(None, current_executable)
