202edb4def6bcef0b36fa31799fc9d49fc604d37
Orsiris de Jong <ozy@netpower.fr>
2023-03-27 16:25:53 +0200

    Switched to new i18nice package
    See https://github.com/Krutyi-4el/i18nice/issues/3

--- i18n/loaders/__init__.py
+++
@@ -1,9 +0,0 @@
-from .loader import Loader, I18nFileLoadError
-from .python_loader import PythonLoader
-from .. import config
-if config.json_available:
-    from .json_loader import JsonLoader
-if config.yaml_available:
-    from .yaml_loader import YamlLoader
-
-del config
