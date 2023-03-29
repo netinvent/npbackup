38e3877f91d12c404bdf497b49eebf9f74c2685d
Orsiris de Jong <ozy@netpower.fr>
2023-03-30 01:57:41 +0200

    Add --no-gui on arm/arm64 builds

--- bin/COMPILE.sh
+++ bin/COMPILE.sh
@@ -2,11 +2,20 @@
 
 # This is an example compiler script
 
+machine="$(printf %.3s "$(uname -m)")"
+
 cd /opt/npbackup
 
 OLD_PYTHONPATH="$PYTHONPATH"
 export PYTHONPATH=/opt/npbackup
 
-/opt/npbackup/venv/bin/python bin/compile.py --audience all
+if [ "$machine" = "arm" ]; then
+        opts=" --no-gui"
+        echo "BUILDING WITHOUT GUI because arm detected"
+else
+        otps=""
+fi
+
+/opt/npbackup/venv/bin/python bin/compile.py --audience all $opts
 
 export PYTHONPATH="$OLD_PYTHONPATH"
