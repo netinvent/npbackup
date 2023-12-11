eec1fe5e991387e9e907320666d440c8e23f833a
Orsiris de Jong <ozy@netpower.fr>
2023-12-11 20:46:24 +0100

    Refactor iter_over_keys into replace_in_iterable

--- npbackup/configuration.py
+++ npbackup/configuration.py
@@ -10,7 +10,9 @@ __license__ = "GPL-3.0-only"
 __build__ = "2023121001"
 __version__ = "2.0.0 for npbackup 2.3.0+"
 
-from typing import Tuple, Optional, List, Callable, Any
+CONF_VERSION = 2.3
+
+from typing import Tuple, Optional, List, Callable, Any, Union
 import sys
 import os
 from copy import deepcopy
@@ -22,7 +24,6 @@ import re
 import platform
 from cryptidy import symmetric_encryption as enc
 from ofunctions.random import random_string
-from ofunctions.misc import deep_dict_update
 from npbackup.customization import ID_STRING
 
 
@@ -91,6 +92,7 @@ ENCRYPTED_OPTIONS = [
 
 # This is what a config file looks like
 empty_config_dict = {
+    "conf_version": CONF_VERSION,
     "repos": {
             "default": {
                 "repo_uri": "",
@@ -191,15 +193,58 @@ def iter_over_keys(d: dict, fn: Callable) -> dict:
     """
     Execute value=fn(value) on any key in a nested env
     """
-    for key, value in d.items():
-        if isinstance(value, dict):
-            d[key] = iter_over_keys(value, fn)
-        else:
-            d[key] = fn(key, d[key])
+    if isinstance(d, dict):
+        for key, value in d.items():
+            if isinstance(value, dict):
+                d[key] = iter_over_keys(value, fn)
+            else:
+                d[key] = fn(key, d[key])
     return d
 
 
-def crypt_config(config: dict, aes_key: str, encrypted_options: List[str], operation: str):
+# TODO: use ofunctions.misc
+def replace_in_iterable(
+    src: Union[dict, list], original: Union[str, Callable], replacement: Any = None, callable_wants_key: bool = False
+):
+    """
+    Recursive replace data in a struct
+
+    Replaces every instance of string original with string replacement in a list/dict
+
+    If original is a callable function, it will replace every instance of original with callable(original)
+    If original is a callable function and callable_wants_key == True,
+      it will replace every instance of original with callable(key, original) for dicts
+      and with callable(original) for any other data type
+    """
+
+    def _replace_in_iterable(key, _src):
+        if isinstance(_src, dict) or isinstance(_src, list):
+            _src = replace_in_iterable(_src, original, replacement, callable_wants_key)
+        elif isinstance(original, Callable):
+            if callable_wants_key:
+                _src = original(key, _src)
+            else:
+                _src = original(_src)
+        elif isinstance(_src, str) and isinstance(replacement, str):
+            _src = _src.replace(original, replacement)
+        else:
+            _src = replacement
+        return _src
+
+    if isinstance(src, dict):
+        for key, value in src.items():
+            src[key] = _replace_in_iterable(key, value)
+    elif isinstance(src, list):
+        result = []
+        for entry in src:
+            result.append(_replace_in_iterable(None, entry))
+        src = result
+    else:
+        src = _replace_in_iterable(None, src)
+    return src
+
+
+def crypt_config(full_config: dict, aes_key: str, encrypted_options: List[str], operation: str):
     try:
         def _crypt_config(key: str, value: Any) -> Any:
             if key in encrypted_options:
@@ -220,13 +265,13 @@ def crypt_config(config: dict, aes_key: str, encrypted_options: List[str], opera
                     raise ValueError(f"Bogus operation {operation} given")
             return value
 
-        return iter_over_keys(config, _crypt_config)
+        return replace_in_iterable(full_config, _crypt_config, callable_wants_key=True)
     except Exception as exc:
-        logger.error(f"Cannot {operation} configuration.")
+        logger.error(f"Cannot {operation} configuration: {exc}.")
         return False
 
 
-def is_encrypted(config: dict) -> bool:
+def is_encrypted(full_config: dict) -> bool:
     is_encrypted = True
 
     def _is_encrypted(key, value) -> Any:
@@ -237,17 +282,17 @@ def is_encrypted(config: dict) -> bool:
                 is_encrypted = True
         return value
 
-    iter_over_keys(config, _is_encrypted)
+    replace_in_iterable(full_config, _is_encrypted, callable_wants_key=True)
     return is_encrypted
 
 
-def has_random_variables(config: dict) -> Tuple[bool, dict]:
+def has_random_variables(full_config: dict) -> Tuple[bool, dict]:
     """
     Replaces ${RANDOM}[n] with n random alphanumeric chars, directly in config dict
     """
     is_modified = False
     
-    def _has_random_variables(key, value) -> Any:
+    def _has_random_variables(value) -> Any:
         nonlocal is_modified
 
         if isinstance(value, str):
@@ -261,79 +306,68 @@ def has_random_variables(config: dict) -> Tuple[bool, dict]:
                 is_modified = True
         return value
     
-    config = iter_over_keys(config, _has_random_variables)
-    return is_modified, config
+    full_config = replace_in_iterable(full_config, _has_random_variables)
+    return is_modified, full_config
 
 
-def evaluate_variables(config: dict, value: str) -> str:
+def evaluate_variables(repo_config: dict, full_config: dict) -> dict:
     """
-    Replaces various variables with their actual value in a string
+    Replace runtime variables with their corresponding value
     """
+    def _evaluate_variables(value):
+        if isinstance(value, str):
+            if "${MACHINE_ID}" in value:
+                machine_id = full_config.g("identity.machine_id")
+                value = value.replace("${MACHINE_ID}", machine_id if machine_id else "")
+            
+            if "${MACHINE_GROUP}" in value:
+                machine_group = full_config.g("identity.machine_group")
+                value = value.replace("${MACHINE_GROUP}", machine_group if machine_group else "")
 
-    # We need to make a loop to catch all nested variables
+            if "${BACKUP_JOB}" in value:
+                backup_job = repo_config.g("backup_opts.backup_job")
+                value = value.replace("${BACKUP_JOB}", backup_job if backup_job else "")
+
+            if "${HOSTNAME}" in value:
+                value = value.replace("${HOSTNAME}", platform.node())
+        return value
+    
+    # We need to make a loop to catch all nested variables (ie variable in a variable)
     # but we also need a max recursion limit
     # If each variable has two sub variables, we'd have max 4x2x2 loops
+    # While this is not the most efficient way, we still get to catch all nested variables
+    # and of course, we don't have thousands of lines to parse, so we're good
     count = 0
     maxcount = 4 * 2 * 2
-    while (
-        "${MACHINE_ID}" in value
-        or "${MACHINE_GROUP}" in value
-        or "${BACKUP_JOB}" in value
-        or "${HOSTNAME}" in value
-    ) and count <= maxcount:
-        value = value.replace("${HOSTNAME}", platform.node())
-
-        try:
-            new_value = config["identity"]["machine_id"]
-        # TypeError may happen if config_dict[x][y] is None
-        except (KeyError, TypeError):
-            new_value = None
-        value = value.replace("${MACHINE_ID}", new_value if new_value else "")
-
-        try:
-            new_value = config["identity"]["machine_group"]
-        # TypeError may happen if config_dict[x][y] is None
-        except (KeyError, TypeError):
-            new_value = None
-        value = value.replace("${MACHINE_GROUP}", new_value if new_value else "")
-
-        try:
-            new_value = config["prometheus"]["backup_job"]
-        # TypeError may happen if config_dict[x][y] is None
-        except (KeyError, TypeError):
-            new_value = None
-        value = value.replace("${BACKUP_JOB}", new_value if new_value else "")
-
+    while count < maxcount:
+        repo_config = replace_in_iterable(repo_config, _evaluate_variables)
         count += 1
-    return value
+    return repo_config
 
 
-def get_repo_config(config: dict, repo_name: str = 'default') -> Tuple[dict, dict]:
+def get_repo_config(full_config: dict, repo_name: str = 'default') -> Tuple[dict, dict]:
     """
-    Created inherited repo config
-    Returns a dict containing the repo config
+    Create inherited repo config
+    Returns a dict containing the repo config, with expanded variables
     and a dict containing the repo interitance status
     """
-
-    def _is_inheritance(key, value):
-        return False
 
     repo_config = ordereddict()
     config_inheritance = ordereddict()
 
     try:
-        repo_config = deepcopy(config.g(f'repos.{repo_name}'))
+        repo_config = deepcopy(full_config.g(f'repos.{repo_name}'))
         # Let's make a copy of config since it's a "pointer object"
-        config_inheritance = iter_over_keys(deepcopy(config.g(f'repos.{repo_name}')), _is_inheritance)
+        config_inheritance = replace_in_iterable(deepcopy(full_config.g(f'repos.{repo_name}')), False)
     except KeyError:
         logger.error(f"No repo with name {repo_name} found in config")
         return None
     try:
-        repo_group = config.g(f'repos.{repo_name}.group')
+        repo_group = full_config.g(f'repos.{repo_name}.group')
     except KeyError:
         logger.warning(f"Repo {repo_name} has no group")
     else:
-        sections = config.g(f'groups.{repo_group}')
+        sections = full_config.g(f'groups.{repo_group}')
         if sections:
             for section in sections:
                 # TODO: ordereddict.g() returns None when key doesn't exist instead of KeyError
@@ -345,75 +379,92 @@ def get_repo_config(config: dict, repo_name: str = 'default') -> Tuple[dict, dic
                 except KeyError:
                     repo_config.s(section, {})
                     config_inheritance.s(section, {})
-                sub_sections = config.g(f'groups.{repo_group}.{section}')
+                sub_sections = full_config.g(f'groups.{repo_group}.{section}')
                 if sub_sections:
                     for entries in sub_sections:
                         # Do not overwrite repo values already present
                         if not repo_config.g(f'{section}.{entries}'):
-                            repo_config.s(f'{section}.{entries}', config.g(f'groups.{repo_group}.{section}.{entries}'))
+                            repo_config.s(f'{section}.{entries}', full_config.g(f'groups.{repo_group}.{section}.{entries}'))
                             config_inheritance.s(f'{section}.{entries}', True)
                         else:
                             config_inheritance.s(f'{section}.{entries}', False)
+
+    repo_config = evaluate_variables(repo_config, full_config)
     return repo_config, config_inheritance
+
+
+def _load_config_file(config_file: Path) -> Union[bool, dict]:
+    """
+    Checks whether config file is valid
+    """
+    try:
+        with open(config_file, "r", encoding="utf-8") as file_handle:
+            yaml = YAML(typ="rt")
+            full_config = yaml.load(file_handle)
+
+            conf_version = full_config.g("conf_version")
+            if conf_version != CONF_VERSION:
+                logger.critical(f"Config file version {conf_version} is not required version {CONF_VERSION}")
+                return False
+            return full_config
+    except OSError:
+        logger.critical(f"Cannot load configuration file from {config_file}")
+        return False
 
 def load_config(config_file: Path) -> Optional[dict]:
     logger.info(f"Loading configuration file {config_file}")
-    try:
-        with open(config_file, "r", encoding="utf-8") as file_handle:
-            # Roundtrip loader is default and preserves comments and ordering
-            yaml = YAML(typ="rt")
-            config = yaml.load(file_handle)
-            config_file_is_updated = False
 
-            # Check if we need to encrypt some variables
-            if not is_encrypted(config):
-                logger.info("Encrypting non encrypted data in configuration file")
-                config_file_is_updated = True
-
-            # Decrypt variables
-            config = crypt_config(config, AES_KEY, ENCRYPTED_OPTIONS, operation='decrypt')
-            if config == False:
-                if EARLIER_AES_KEY:
-                    logger.warning("Trying to migrate encryption key")
-                    config = crypt_config(config, EARLIER_AES_KEY, ENCRYPTED_OPTIONS, operation='decrypt')
-                    if config == False:
-                        logger.critical("Cannot decrypt config file with earlier key")
-                        sys.exit(12)
-                    else:
-                        config_file_is_updated = True
-                        logger.warning("Successfully migrated encryption key")
-                else:
-                    logger.critical("Cannot decrypt config file")
-                    sys.exit(11)
-
-
-            # Check if we need to expand random vars
-            is_modified, config = has_random_variables(config)
-            if is_modified:
-                config_file_is_updated = True
-                logger.info("Handling random variables in configuration files")
-
-            # save config file if needed
-            if config_file_is_updated:
-                logger.info("Updating config file")
-                save_config(config_file, config)
-
-            return config
-
-    except OSError:
-        logger.critical(f"Cannot load configuration file from {config_file}")
+    full_config = _load_config_file(config_file)
+    if not full_config:
         return None
+    config_file_is_updated = False
+    print(full_config)
+
+    # Check if we need to encrypt some variables
+    if not is_encrypted(full_config):
+        logger.info("Encrypting non encrypted data in configuration file")
+        config_file_is_updated = True
+    print(full_config)
+    # Decrypt variables
+    full_config = crypt_config(full_config, AES_KEY, ENCRYPTED_OPTIONS, operation='decrypt')
+    if full_config == False:
+        if EARLIER_AES_KEY:
+            logger.warning("Trying to migrate encryption key")
+            full_config = crypt_config(full_config, EARLIER_AES_KEY, ENCRYPTED_OPTIONS, operation='decrypt')
+            if full_config == False:
+                logger.critical("Cannot decrypt config file with earlier key")
+                sys.exit(12)
+            else:
+                config_file_is_updated = True
+                logger.warning("Successfully migrated encryption key")
+        else:
+            logger.critical("Cannot decrypt config file")
+            sys.exit(11)
 
 
-def save_config(config_file: Path, config: dict) -> bool:
+    # Check if we need to expand random vars
+    is_modified, full_config = has_random_variables(full_config)
+    if is_modified:
+        config_file_is_updated = True
+        logger.info("Handling random variables in configuration files")
+
+    # save config file if needed
+    if config_file_is_updated:
+        logger.info("Updating config file")
+        save_config(config_file, full_config)
+
+    return full_config
+
+
+def save_config(config_file: Path, full_config: dict) -> bool:
     try:
         with open(config_file, "w", encoding="utf-8") as file_handle:
-            if not is_encrypted(config):
-                config = crypt_config(config, AES_KEY, ENCRYPTED_OPTIONS, operation='encrypt')
+            if not is_encrypted(full_config):
+                full_config = crypt_config(full_config, AES_KEY, ENCRYPTED_OPTIONS, operation='encrypt')
             yaml = YAML(typ="rt")
-            yaml.dump(config, file_handle)
+            yaml.dump(full_config, file_handle)
         # Since yaml is a "pointer object", we need to decrypt after saving
-        config = crypt_config(config, AES_KEY, ENCRYPTED_OPTIONS, operation='decrypt')
+        full_config = crypt_config(full_config, AES_KEY, ENCRYPTED_OPTIONS, operation='decrypt')
         return True
     except OSError:
         logger.critical(f"Cannot save configuration file to {config_file}")
