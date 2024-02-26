688f6e766837b75f0cdc66fc60648a05bf0453b7
Orsiris de Jong <ozy@netpower.fr>
2024-02-26 14:42:33 +0100

    Don't build no-gui versions anymore

--- bin/COMPILE.sh
+++ bin/COMPILE.sh
@@ -9,14 +9,6 @@ cd /opt/npbackup
 OLD_PYTHONPATH="$PYTHONPATH"
 export PYTHONPATH=/opt/npbackup
 
-if [ "$(printf %.3s $machine)" = "arm" ] || [ "$machine" = "aarch64" ]; then
-        opts=" --no-gui"
-        echo "BUILDING WITHOUT GUI because arm detected"
-else
-        otps=""
-        echo "BUILDING WITH GUI"
-fi
-
 /opt/npbackup/venv/bin/python bin/compile.py --audience all $opts
 
 export PYTHONPATH="$OLD_PYTHONPATH"
