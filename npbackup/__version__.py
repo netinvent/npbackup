b4dbba087d963ca3f39958dfdd3a921d3b9a88ec
deajan <ozy@netpower.fr>
2024-04-16 01:14:27 +0200

    Bump version

--- npbackup/__version__.py
+++ npbackup/__version__.py
@@ -9,8 +9,8 @@ __site__ = "https://www.netperfect.fr/npbackup"
 __description__ = "NetPerfect Backup Client"
 __copyright__ = "Copyright (C) 2022-2024 NetInvent"
 __license__ = "GPL-3.0-only"
-__build__ = "2024031001"
-__version__ = "3.0.0-alpha3"
+__build__ = "2024041601"
+__version__ = "3.0.0-alpha4"
 
 
 import sys
