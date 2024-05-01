011d99bde486a32071ea439f9a1ea187aa4086a2
Orsiris de Jong <ozy@netpower.fr>
2024-05-01 19:30:39 +0200

    Make windows EV signer use new paths

--- bin/sign_windows.py
+++ bin/sign_windows.py
@@ -7,8 +7,8 @@ __intname__ = "npbackup.sign_windows"
 __author__ = "Orsiris de Jong"
 __copyright__ = "Copyright (C) 2023-2024 NetInvent"
 __license__ = "GPL-3.0-only"
-__build__ = "2023050301"
-__version__ = "1.0.0"
+__build__ = "2024050101"
+__version__ = "1.1.0"
 
 
 import os
@@ -21,16 +21,20 @@ except ImportError:
 basepath = r"C:\GIT\npbackup\BUILDS"
 audiences = ["private", "public"]
 arches = ["x86", "x64"]
-binaries = ["NPBackup.exe", "NPBackupInstaller.exe"]
+binaries = ["npbackup-cli", "npbackup-gui", "npbackup-viewer", "NPBackupInstaller"]
 
 signer = SignTool()
 
 for audience in audiences:
     for arch in arches:
         for binary in binaries:
-            exe_path = os.path.join(basepath, audience, "windows", arch, binary)
-            result = signer.sign(exe_path, bitness=arch)
-            if not result:
-                raise EnvironmentError(
-                    "Could not sign executable ! Is the PKI key connected ?"
-                )
+            one_file_exe_path = exe_path = os.path.join(basepath, audience, "windows", arch, binary + f"-{arch}.exe")
+            standalone_exe_path = os.path.join(basepath, audience, "windows", arch, binary + ".dist", binary + f"-{arch}.exe")
+            for exe_file in (one_file_exe_path, standalone_exe_path):
+                if os.path.isfile(exe_file):
+                    print(f"Signing {exe_file}")
+                    result = signer.sign(exe_file, bitness=arch)
+                    if not result:
+                        raise EnvironmentError(
+                            "Could not sign executable ! Is the PKI key connected ?"
+                        )
