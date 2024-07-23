c67bdb9c9c45bd66629f2c83f4ff1792d7292f0b
deajan <ozy@netpower.fr>
2024-07-23 14:38:56 +0200

    Add permissions to restic upgrader

--- RESTIC_SOURCE_FILES/update_restic.sh
+++ RESTIC_SOURCE_FILES/update_restic.sh
@@ -7,6 +7,7 @@ export REPO=restic
 LATEST_VERSION=$(curl -s https://api.github.com/repos/${ORG}/${REPO}/releases/latest | grep "tag_name" | cut -d'"' -f4)
 echo Latest restic version ${LATEST_VERSION}
 
+errors=false
 for platform in "windows_386" "windows_amd64" "linux_arm" "linux_arm64" "linux_amd64" "darwin_amd64" "freebsd_amd64"; do
     	restic_filename="restic_${LATEST_VERSION//v}_${platform}"
 	    if [ ! -f "${restic_filename}" ]; then
@@ -22,23 +23,29 @@ for platform in "windows_386" "windows_amd64" "linux_arm" "linux_arm64" "linux_a
         	curl -OL https://github.com/restic/restic/releases/download/${LATEST_VERSION}/restic_${LATEST_VERSION//v}_{$platform}.${ext}
 	        if [ $? -ne 0 ]; then
         	    echo "Failed to download ${restic_filename}"
-	            	exit 1
+	            	errors=true
         	else
                 	if [ -f "${restic_filename}.bz2" ]; then
             			bzip2 -d "${restic_filename}.bz2"
+						chmod +x "${restic_filename}"
 			elif [ -f "${restic_filename}.zip" ]; then
 				unzip "${restic_filename}.zip"
 			else
 				echo "Archive ${restic_filename} not found"
-				exit 1
+				errors=true
 			fi
             		if [ $? -ne 0 ]; then
                 		echo "Failed to decompress ${restic_filename}.bz2"
-                		exit 1
+                		errors=true
         		fi
 			[ -f "${restic_filename}.zip" ] && rm -f "${restic_filename}.zip"
     		fi
 	fi
 done
 
-echo "Finished updating restic binaries"
+if [ "${errors}" = true ]; then
+	echo "Errors occurred during update"
+	exit 1
+else
+	echo "Finished updating restic binaries"
+fi
