e83234e34128c5b2736713d3b99b69a654c71702
deajan <ozy@netpower.fr>
2024-09-06 12:48:34 +0200

    Bump version to RC5

--- npbackup/__version__.py
+++ npbackup/__version__.py
@@ -9,8 +9,8 @@ __site__ = "https://www.netperfect.fr/npbackup"
 __description__ = "NetPerfect Backup Client"
 __copyright__ = "Copyright (C) 2022-2024 NetInvent"
 __license__ = "GPL-3.0-only"
-__build__ = "2024090101"
-__version__ = "3.0.0-rc4"
+__build__ = "2024090601"
+__version__ = "3.0.0-rc5"
 
 
 import sys
