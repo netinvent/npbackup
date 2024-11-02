b2c5140329c39d91a69f916eacfac72cf7119477
Orsiris de Jong <ozy@netpower.fr>
2024-11-02 13:47:42 +0100

    Improve group runner log message wording

--- npbackup/core/runner.py
+++ npbackup/core/runner.py
@@ -1688,7 +1688,7 @@ class NPBackupRunner:
             if not result:
                 group_result = False
             metric_writer(repo_config, result, None, operation, self.dry_run)
-        self.write_logs("Finished execution group operations", level="info")
+        self.write_logs("Finished execution of group operations", level="info")
         if self.json_output:
             js["result"] = group_result
             return js
