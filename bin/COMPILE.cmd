29027cf11dcc143183a70384e01a323192e8274e
deajan <ozy@netpower.fr>
2024-12-10 23:57:54 +0100

    windows build script should not close cmd

--- bin/COMPILE.cmd
+++ bin/COMPILE.cmd
@@ -7,7 +7,7 @@ SET PYTHON32=c:\python37-32\python.exe
 
 
 cd C:\GIT\npbackup
-git pull || exit 1
+git pull || GOTO ERROR
 
 :: cd RESTIC_SOURCE_FILES
 :: cat update_restic.sh | dos2unix | sh
@@ -17,18 +17,21 @@ git pull || exit 1
 SET OLD_PYTHONPATH=%PYTHONPATH%
 SET PYTHONPATH=c:\GIT\npbackup
 
-%PYTHON64% RESTIC_SOURCE_FILES/update_restic.py || exit 1
+%PYTHON64% RESTIC_SOURCE_FILES/update_restic.py || GOTO ERROR
 
 %PYTHON64% -m pip install pytest
-%PYTHON64% -m pytest /opt/npbackup/tests || exit 1
+%PYTHON64% -m pytest /opt/npbackup/tests || GOTO ERROR
 
-"%PYTHON64%" -m pip install --upgrade -r npbackup/requirements.txt || exit 1
+"%PYTHON64%" -m pip install --upgrade -r npbackup/requirements.txt || GOTO ERROR
 "%PYTHON64%" bin\compile.py --audience all --sign "C:\ODJ\KEYS\NetInventEV.dat"
 
 
-"%PYTHON32%" -m pip install --upgrade -r npbackup/requirements.txt || exit 1
+"%PYTHON32%" -m pip install --upgrade -r npbackup/requirements.txt || GOTO ERROR
 "%PYTHON32%" bin\compile.py --audience all --sign "C:\ODJ\KEYS\NetInventEV.dat"
 
+:ERROR
+echo "Failed to run build script"
+:END
 SET PYTHONPATH=%OLD_PYTHONPATH%
 
 
