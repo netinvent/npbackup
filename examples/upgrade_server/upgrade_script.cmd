712aaf4b3e0a07bb6c9634100a1abf17269509f7
deajan <ozy@netpower.fr>
2025-01-24 20:50:54 +0100

    upgrade_server: Fix windows script

--- examples/upgrade_server/upgrade_script.cmd
+++ examples/upgrade_server/upgrade_script.cmd
@@ -30,14 +30,12 @@ echo "Loading new executable {CURRENT_EXECUTABLE} --check-config {original_args}
 "{CURRENT_EXECUTABLE}" --check-config {original_args} >> "{log_file}" 2>&1
 IF !ERRORLEVEL! NEQ 0 (
     echo "New executable failed. Rolling back" >> "{log_file}" 2>&1
-    IF "%REPLACE_METHOD%"=="overwrite" (
-        echo "Overwrite method used. Overwrite back" >> "{log_file}" 2>&1
-        xcopy /S /Y /I "{backup_dist}\*" "{CURRENT_DIR}" >> "{log_file}" 2>&1
-    ) ELSE (
-        echo "Move method used. Move back" >> "{log_file}" 2>&1
-        rd /S /Q "{CURRENT_DIR}" >> "{log_file}" 2>&1 &
-        move /Y "{backup_dist}" "{CURRENT_DIR}" >> "{log_file}" 2>&1
-    )
+    IF "%REPLACE_METHOD%"=="overwrite" echo "Overwrite method used. Overwrite back" >> "{log_file}" 2>&1
+    IF "%REPLACE_METHOD%"=="overwrite" xcopy /S /Y /I "{backup_dist}\*" "{CURRENT_DIR}" >> "{log_file}" 2>&1
+
+    IF "%REPLACE_METHOD%"!="overwrite" echo "Move method used. Move back" >> "{log_file}" 2>&1
+    IF "%REPLACE_METHOD%"!="overwrite" rd /S /Q "{CURRENT_DIR}" >> "{log_file}" 2>&1 &
+    IF "%REPLACE_METHOD%"!="overwrite" move /Y "{backup_dist}" "{CURRENT_DIR}" >> "{log_file}" 2>&1
 ) ELSE (
     echo "Upgrade successful" >> "{log_file}" 2>&1
     rd /S /Q "{backup_dist}" >> "{log_file}" 2>&1
