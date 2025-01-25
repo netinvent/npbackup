b298ffdea20a0a4e8dc8a203a0587c69221af858
deajan <ozy@netpower.fr>
2025-01-25 13:31:31 +0100

    upgrade_client: Make inline script more readable on debug

--- npbackup/upgrade_client/upgrader.py
+++ npbackup/upgrade_client/upgrader.py
@@ -352,40 +352,40 @@ def auto_upgrader(
         if os.name == "nt":
             cmd = (
                 f"setlocal EnableDelayedExpansion &"
-                f'echo "Launching upgrade" >> "{log_file}" 2>&1 &'
-                f'echo "Moving current dist from {CURRENT_DIR} to {backup_dist}" >> "{log_file}" 2>&1 &'
-                f'move /Y "{CURRENT_DIR}" "{backup_dist}" >> "{log_file}" 2>&1 &'
+                f'echo "Launching upgrade" >> "{log_file}" 2>&1 & '
+                f'echo "Moving current dist from {CURRENT_DIR} to {backup_dist}" >> "{log_file}" 2>&1 & '
+                f'move /Y "{CURRENT_DIR}" "{backup_dist}" >> "{log_file}" 2>&1 & '
                 f"IF !ERRORLEVEL! NEQ 0 ( "
-                f'    echo "Moving current dist failed. Trying to copy it." >> "{log_file}" 2>&1 &'
-                rf'    xcopy /S /Y /I "{CURRENT_DIR}\*" "{backup_dist}" >> "{log_file}" 2>&1 &'
-                f'    echo "Now trying to overwrite current dist with upgrade dist" >> "{log_file}" 2>&1 &'
-                rf'    xcopy /S /Y /I "{upgrade_dist}\*" "{CURRENT_DIR}" >> "{log_file}" 2>&1 &'
+                f'    echo "Moving current dist failed. Trying to copy it." >> "{log_file}" 2>&1 & '
+                rf'    xcopy /S /Y /I "{CURRENT_DIR}\*" "{backup_dist}" >> "{log_file}" 2>&1 & '
+                f'    echo "Now trying to overwrite current dist with upgrade dist" >> "{log_file}" 2>&1 & '
+                rf'    xcopy /S /Y /I "{upgrade_dist}\*" "{CURRENT_DIR}" >> "{log_file}" 2>&1 & '
                 f"    set REPLACE_METHOD=overwrite "
                 f") ELSE ( "
-                f'    echo "Moving upgraded dist from {upgrade_dist} to {CURRENT_DIR}" >> "{log_file}" 2>&1 &'
-                f'    move /Y "{upgrade_dist}" "{CURRENT_DIR}" >> "{log_file}" 2>&1 &'
-                f'    echo "Copying optional configuration files from {backup_dist} to {CURRENT_DIR}" >> "{log_file}" 2>&1 &'
-                rf'    xcopy /S /Y /I "{backup_dist}\*conf" {CURRENT_DIR} > NUL 2>&1 &'
+                f'    echo "Moving upgraded dist from {upgrade_dist} to {CURRENT_DIR}" >> "{log_file}" 2>&1 & '
+                f'    move /Y "{upgrade_dist}" "{CURRENT_DIR}" >> "{log_file}" 2>&1 & '
+                f'    echo "Copying optional configuration files from {backup_dist} to {CURRENT_DIR}" >> "{log_file}" 2>&1 & '
+                rf'    xcopy /S /Y /I "{backup_dist}\*conf" {CURRENT_DIR} > NUL 2>&1 & '
                 f"    set REPLACE_METHOD=move "
                 f")  &"
-                f'echo "Loading new executable {CURRENT_EXECUTABLE} --run-as-cli --check-config {original_args}" >> "{log_file}" 2>&1 &'
-                f'"{CURRENT_EXECUTABLE}" --run-as-cli --check-config {original_args} >> "{log_file}" 2>&1 &'
+                f'echo "Loading new executable {CURRENT_EXECUTABLE} --run-as-cli --check-config {original_args}" >> "{log_file}" 2>&1 & '
+                f'"{CURRENT_EXECUTABLE}" --run-as-cli --check-config {original_args} >> "{log_file}" 2>&1 & '
                 f"IF !ERRORLEVEL! NEQ 0 ( "
-                f'    echo "New executable failed. Rolling back" >> "{log_file}" 2>&1 &'
-                f'    IF "%REPLACE_METHOD%"=="overwrite" echo "Overwrite method used. Overwrite back" >> "{log_file}" 2>&1 &'
-                rf'    IF "%REPLACE_METHOD%"=="overwrite" xcopy /S /Y /I "{backup_dist}\*" "{CURRENT_DIR}" >> "{log_file}" 2>&1 &'
-                f'    IF NOT "%REPLACE_METHOD%"=="overwrite" echo "Move method used. Move back" >> "{log_file}" 2>&1 &'
-                f'    IF NOT "%REPLACE_METHOD%"=="overwrite" move /Y "{CURRENT_DIR}" "{backup_dist}.original" >> "{log_file}" 2>&1 &'
+                f'    echo "New executable failed. Rolling back" >> "{log_file}" 2>&1 & '
+                f'    IF "%REPLACE_METHOD%"=="overwrite" echo "Overwrite method used. Overwrite back" >> "{log_file}" 2>&1 & '
+                rf'    IF "%REPLACE_METHOD%"=="overwrite" xcopy /S /Y /I "{backup_dist}\*" "{CURRENT_DIR}" >> "{log_file}" 2>&1 & '
+                f'    IF NOT "%REPLACE_METHOD%"=="overwrite" echo "Move method used. Move back" >> "{log_file}" 2>&1 & '
+                f'    IF NOT "%REPLACE_METHOD%"=="overwrite" move /Y "{CURRENT_DIR}" "{backup_dist}.original" >> "{log_file}" 2>&1 & '
                 f'    IF NOT "%REPLACE_METHOD%"=="overwrite" move /Y "{backup_dist}" "{CURRENT_DIR}" >> "{log_file}" 2>&1 '
                 f") ELSE ( "
-                f'    echo "Upgrade successful" >> "{log_file}" 2>&1 &'
-                f'    rd /S /Q "{backup_dist}" >> "{log_file}" 2>&1 &'
-                f'    rd /S /Q "{upgrade_dist}" > NUL 2>&1 &'
+                f'    echo "Upgrade successful" >> "{log_file}" 2>&1 & '
+                f'    rd /S /Q "{backup_dist}" >> "{log_file}" 2>&1 & '
+                f'    rd /S /Q "{upgrade_dist}" > NUL 2>&1 & '
                 f'    del /F /S /Q "{downloaded_archive}" >> "{log_file}" 2>&1 '
                 f") &"
-                f'echo "Running as initially planned:" >> "{log_file}" 2>&1 &'
-                f'echo "{CURRENT_EXECUTABLE} {original_args}" >> "{log_file}" 2>&1 &'
-                f'"{CURRENT_EXECUTABLE}" {original_args} &'
+                f'echo "Running as initially planned:" >> "{log_file}" 2>&1 & '
+                f'echo "{CURRENT_EXECUTABLE} {original_args}" >> "{log_file}" 2>&1 & '
+                f'"{CURRENT_EXECUTABLE}" {original_args} & '
                 f'echo "Upgrade script run finished" >> "{log_file}" 2>&1 '
             )
         else:
@@ -426,5 +426,6 @@ def auto_upgrader(
         log_file,
     )
     logger.debug(cmd)
+    # WIP
     # deferred_command(cmd, defer_time=UPGRADE_DEFER_TIME)
     sys.exit(0)
