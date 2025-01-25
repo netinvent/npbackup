842f9915c81a581a80c6962f2eb0559dc8334b7e
deajan <ozy@netpower.fr>
2025-01-25 13:14:31 +0100

    upgrade_server: Show what filess are getting served

--- upgrade_server/upgrade_server/crud.py
+++ upgrade_server/upgrade_server/crud.py
@@ -164,6 +164,7 @@ def get_file(
             return False
         return unknown_artefact
 
+    logger.info(f"Serving {file.artefact.value} file in {artefact_path}")
     with open(artefact_path, "rb") as fh:
         file_content_bytes = fh.read()
         if content:
