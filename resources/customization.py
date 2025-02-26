30e7527c69e3176f243c41293ca44e2f056e30d1
deajan <ozy@netpower.fr>
2025-02-26 11:54:45 +0100

    Re-organisze customization file

--- resources/customization.py
+++ resources/customization.py
@@ -7,7 +7,7 @@ __intname__ = "npbackup.customization"
 __author__ = "Orsiris de Jong"
 __copyright__ = "Copyright (C) 2022-2025 NetInvent"
 __license__ = "GPL-3.0-only"
-__build__ = "2024010301"
+__build__ = "2025022601"
 __version__ = "1.3.1"
 
 
@@ -15,9 +15,12 @@ import os
 
 
 # Windows Program name for scheduled task
-PROGRAM_NAME = "Client Sauvegarde NetPerfect"
+PROGRAM_NAME = "NPBackup Client"
 # Windows target directory
 PROGRAM_DIRECTORY = "NetPerfect Backup Client"
+# Task names
+TASK_AUTHOR = "NetInvent Opensource Initiative"
+TASK_URI = f"\\{PROGRAM_NAME}"
 
 # Windows executable file info
 COMPANY_NAME = "NetInvent"
@@ -25,11 +28,12 @@ COPYRIGHT = "NetInvent 2022-2025"
 FILE_DESCRIPTION = "Network Backup Client"
 TRADEMARKS = "NetInvent (C)"
 PRODUCT_NAME = "NPBackup Network Backup Client"
-SHORT_PRODUCT_NAME = "NPBackup"
 
-# Task names
-TASK_AUTHOR = "NetPerfect Opensource Initiative"
-TASK_URI = f"\\{PROGRAM_NAME}"
+#################################################################################################################
+# YOU ARE ALLOWED TO CHANGE ANY SETTINGS BELOW THIS LINE TO WHATEVER BRANDING YOU WANT, EXCEPT THE LICENSE TEXT #
+#################################################################################################################
+
+SHORT_PRODUCT_NAME = "NPBackup"
 
 # SimpleGUI theme
 # Valid list: ['Black', 'BlueMono', 'BluePurple', 'BrightColors', 'BrownBlue', 'Dark', 'Dark2', 'DarkAmber', 'DarkBlack', 'DarkBlack1', 'DarkBlue', 'DarkBlue1', 'DarkBlue10', 'DarkBlue11', 'DarkBlue12', 'DarkBlue13', 'DarkBlue14', 'DarkBlue15', 'DarkBlue16', 'DarkBlue17', 'DarkBlue2', 'DarkBlue3', 'DarkBlue4', 'DarkBlue5', 'DarkBlue6', 'DarkBlue7', 'DarkBlue8', 'DarkBlue9', 'DarkBrown', 'DarkBrown1', 'DarkBrown2', 'DarkBrown3', 'DarkBrown4', 'DarkBrown5', 'DarkBrown6', 'DarkBrown7', 'DarkGreen', 'DarkGreen1', 'DarkGreen2', 'DarkGreen3', 'DarkGreen4', 'DarkGreen5', 'DarkGreen6', 'DarkGreen7', 'DarkGrey', 'DarkGrey1', 'DarkGrey10', 'DarkGrey11', 'DarkGrey12', 'DarkGrey13', 'DarkGrey14', 'DarkGrey15', 'DarkGrey2', 'DarkGrey3', 'DarkGrey4', 'DarkGrey5', 'DarkGrey6', 'DarkGrey7', 'DarkGrey8', 'DarkGrey9', 'DarkPurple', 'DarkPurple1', 'DarkPurple2', 'DarkPurple3', 'DarkPurple4', 'DarkPurple5', 'DarkPurple6', 'DarkPurple7', 'DarkRed', 'DarkRed1', 'DarkRed2', 'DarkTanBlue', 'DarkTeal', 'DarkTeal1', 'DarkTeal10', 'DarkTeal11', 'DarkTeal12', 'DarkTeal2', 'DarkTeal3', 'DarkTeal4', 'DarkTeal5', 'DarkTeal6', 'DarkTeal7', 'DarkTeal8', 'DarkTeal9', 'Default', 'Default1', 'DefaultNoMoreNagging', 'GrayGrayGray', 'Green', 'GreenMono', 'GreenTan', 'HotDogStand', 'Kayak', 'LightBlue', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4', 'LightBlue5', 'LightBlue6', 'LightBlue7', 'LightBrown', 'LightBrown1', 'LightBrown10', 'LightBrown11', 'LightBrown12', 'LightBrown13', 'LightBrown2', 'LightBrown3', 'LightBrown4', 'LightBrown5', 'LightBrown6', 'LightBrown7', 'LightBrown8', 'LightBrown9', 'LightGray1', 'LightGreen', 'LightGreen1', 'LightGreen10', 'LightGreen2', 'LightGreen3', 'LightGreen4', 'LightGreen5', 'LightGreen6', 'LightGreen7', 'LightGreen8', 'LightGreen9', 'LightGrey', 'LightGrey1', 'LightGrey2', 'LightGrey3', 'LightGrey4', 'LightGrey5', 'LightGrey6', 'LightPurple', 'LightTeal', 'LightYellow', 'Material1', 'Material2', 'NeutralBlue', 'Purple', 'Python', 'PythonPlus', 'Reddit', 'Reds', 'SandyBeach', 'SystemDefault', 'SystemDefault1', 'SystemDefaultForReal', 'Tan', 'TanBlue', 'TealMono', 'Topanga']
