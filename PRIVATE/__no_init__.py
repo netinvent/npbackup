907a3ce616d2e4bf9d4cd60a482046207125298d
deajan <ozy@netpower.fr>
2025-02-26 11:45:30 +0100

    Add more PRIVATE directory context

--- PRIVATE/__no_init__.py
+++ PRIVATE/__no_init__.py
@@ -3,3 +3,11 @@
 
 # Placeholder to remind this directory should not become a package
 # In order to avoid bundling it with the main package as wheel / bdist
+
+# THIS DIRECTORY MAY CONTAIN OVERRIDE FILES
+# 
+# 
+#  `_private_secret_keys.py` here will override secret_keys.py
+#  `_obfuscation.py` here will override obfuscation.py
+#
+# If these files exist, build type will become "private" instead of "public"
