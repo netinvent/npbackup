3704eb26838584f1f87556dc73d9bde3663198ee
deajan <ozy@netpower.fr>
2025-04-18 22:41:28 +0200

    Fix compiling on legacy linux

--- npbackup/core/restic_source_binary.py
+++ npbackup/core/restic_source_binary.py
@@ -26,7 +26,7 @@ def get_restic_internal_binary(arch: str) -> str:
     binary = None
     if os.path.isdir(RESTIC_SOURCE_FILES_DIR):
         if os.name == "nt":
-            if IS_LEGACY:
+            if IS_LEGACY or "legacy" in "arch":
                 # Last compatible restic binary for Windows 7, see https://github.com/restic/restic/issues/5065
                 # We build a legacy version of restic for windows 7 and Server 2008R2
                 logger.info(
@@ -40,20 +40,24 @@ def get_restic_internal_binary(arch: str) -> str:
                 binary = "restic_*_windows_386.exe"
             else:
                 binary = "restic_*_windows_amd64.exe"
-        elif sys.platform.lower() == "darwin":
-            if arch == "arm64":
-                binary = "restic_*_darwin_arm64"
-            else:
-                binary = "restic_*_darwin_amd64"
         else:
-            if arch == "arm":
-                binary = "restic_*_linux_arm"
-            elif arch == "arm64":
-                binary = "restic_*_linux_arm64"
-            elif arch == "x64":
-                binary = "restic_*_linux_amd64"
+            # We don't have restic legacy builds for unixes
+            # so we can drop the -legacy suffix
+            arch = arch.replace("-legacy", "")
+            if sys.platform.lower() == "darwin":
+                if arch == "arm64":
+                    binary = "restic_*_darwin_arm64"
+                else:
+                    binary = "restic_*_darwin_amd64"
             else:
-                binary = "restic_*_linux_386"
+                if arch == "arm":
+                    binary = "restic_*_linux_arm"
+                elif arch == "arm64":
+                    binary = "restic_*_linux_arm64"
+                elif arch == "x64":
+                    binary = "restic_*_linux_amd64"
+                else:
+                    binary = "restic_*_linux_386"
     if binary:
         guessed_path = glob.glob(os.path.join(RESTIC_SOURCE_FILES_DIR, binary))
         if guessed_path:
