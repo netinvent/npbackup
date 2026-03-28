95593e929fd2fbae9ab60afc5a085e3c78f041ea
deajan <ozy@netpower.fr>
2026-03-28 19:18:32 +0100

    compiler: Use default executable name whenever we can't find one

--- bin/compile.py
+++ bin/compile.py
@@ -159,13 +159,14 @@ def _compile(
 
     # Try to get alternative executable name
     audience_customization = os.path.join(BASEDIR, "..", "PRIVATE", audience, "_customization.py")
+    
+    executable_name = "npbackup"
     if os.path.isfile(audience_customization):
         executable_name = extract_executable_name(audience_customization)
         if executable_name:
             print(f"Found audience specific executable name {executable_name} for audience {audience}")
         else:
             print("Using default executable name")
-            executable_name = "npbackup"
 
     initial_source_program = "bin/npbackup-{}".format(build_type)
     audience_source_program = "bin/{}-{}".format(executable_name, build_type)
