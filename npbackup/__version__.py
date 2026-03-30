2f1f0bb4fb427033f0cbcda325ec278bc7a4febb
deajan <ozy@netpower.fr>
2026-03-30 23:05:26 +0200

    Bump build number

--- npbackup/__version__.py
+++ npbackup/__version__.py
@@ -9,7 +9,7 @@ __site__ = "https://www.netperfect.fr/npbackup"
 __description__ = "NetPerfect Backup Client"
 __copyright__ = "Copyright (C) 2022-2026 NetInvent"
 __license__ = "GPL-3.0-only"
-__build__ = "2026032001"
+__build__ = "2026033001"
 __version__ = "3.1.0-beta6"
 
 
